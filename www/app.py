4c6be64d7698176c5046064f669778bfa88fae40
zhengsf <ozhengsfo@163.com>
2019-01-03 23:35:28 +0800

    second day 编写web app 骨架 使用aiohttp

---
+++ www/app.py
@@ -0,0 +1,33 @@
+#encoding:utf-8
+"""
+@project = webapp
+@file = app.py
+@author = zhengsf
+@create_time = 2019/1/3 23:18
+"""
+__author__ = 'zhengsf'
+'''
+async web application
+'''
+
+import logging;logging.basicConfig(level=logging.INFO)
+
+import asyncio,os,json,time
+
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>',headers={'content-type':'text/html'})
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET','/',index)
+    srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    logging.info('server started at http://127.0.0.1:9000')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
